e6180aba0b8e08bc3b3587afac663b22f4c24d0e
Arthur <acvignon@kth.se>
2019-01-25 12:58:01 +0100

    #10 added a description to the tests

--- src/test/LIC5Test.java
+++ src/test/LIC5Test.java
@@ -6,6 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class LIC5Test {
 
+	/**
+	 * The LIC should be satisfied if there exists at least two consecutive data points (X[i],Y[i]) and (X[j],Y[j]), 
+	 * such that X[j] - X[i] < 0. (where i = j-1)
+	 * The first four tests are basic tests with values which work or not and some border cases.
+	 * This one has only incrementing Xs so it should return false.
+	 */
 	@Test
 	void testClassicFalse() {
 		Decide.NUM_POINTS = 10;
@@ -15,7 +21,9 @@ class LIC5Test {
 		Decide.Y = y;
 		assertFalse(Decide.LIC5());
 	}
-
+	/**
+	 * This test should return true as X[2]>X[3]
+	 */
     @Test
 	void testClassicTrue() {
 		Decide.NUM_POINTS = 10;
@@ -25,7 +33,9 @@ class LIC5Test {
 		Decide.Y = y;
 		assertTrue(Decide.LIC5());
 	}
-
+    /**
+     * This test checks border cases when values are equal. it should return false.
+     */
     @Test
 	void testBorderCaseFalse() {
 		Decide.NUM_POINTS = 10;
@@ -35,17 +45,9 @@ class LIC5Test {
 		Decide.Y = y;
 		assertFalse(Decide.LIC5());
 	}
-
-    @Test
-	void testBorderCaseTrue() {
-		Decide.NUM_POINTS = 10;
-		double x[] = {1,0.999999,2,2,3,3,4,4,5,5};
-		double y[] = {1,2,3,4,5,6,7,8,9,0};
-		Decide.X = x;
-		Decide.Y = y;
-		assertTrue(Decide.LIC5());
-	}
-
+    /**
+     * Here we check that LIC5() returns false when there aren't enough points.
+     */
     @Test
     void notEnoughPoints() {
         Decide.NUM_POINTS = 1;
@@ -55,7 +57,11 @@ class LIC5Test {
         Decide.Y = y;
         assertFalse(Decide.LIC5());
     }
-
+    /**
+     * We check that when incorrect data is entered, the method throws the right exception.
+	 * In this case we give a NUM_POINTS higher than the real number of points.
+	 * We expect an IndexOutOfBoundsException.
+     */
 	@Test()
 	void throwsIndexOutOfBoundsException() {
 		Decide.NUM_POINTS = 4;
