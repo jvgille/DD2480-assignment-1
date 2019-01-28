4ba1aa72dbe4d1de4b38b9a1ef0724bf590fd489
Axel <axellaru@kth.se>
2019-01-28 12:35:34 +0100

    add test to LIC1 issue/65

---
+++ src/test/LIC1Test.java
@@ -0,0 +1,42 @@
+package main;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import main.*;
+
+public class LIC1Test {
+    /*
+	There exists at least one set of three consecutive data points that 
+    cannot all be contained within or on a circle of radius RADIUS1.
+	(0 ≤ RADIUS1)
+	 */
+	@Test
+	public void test1() {
+		Parameters p = new Parameters();
+        double[] x = { 0, 1, 2, 3, 4 }; // coordinates of our points
+        double[] y = { 0, 1, 2, 3, 4 };
+		Decide.NUM_POINTS = x.length;
+        Decide.X = x;
+		Decide.Y = y;
+		p.radius1 = -1;
+		Decide.PARAMETERS = p;
+		
+        assertFalse(Decide.LIC1()); //should fail (radius<0)
+	}
+
+	@Test
+	public void test2() {
+		Parameters p = new Parameters();
+        double[] x = { 1, 6, 3, 4 }; // coordinates of our points
+        double[] y = { 1, 2, 9, 4 };
+		Decide.NUM_POINTS = x.length;
+        Decide.X = x;
+		Decide.Y = y;
+		p.radius1 = 6;
+		Decide.PARAMETERS = p;
+		
+		assertTrue(Decide.LIC1()); //should pass.
+	}
+}
